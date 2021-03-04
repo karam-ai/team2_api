bac95ce7c0eb331d9dba4be8ea57e96a1db69f86
karam qusai <483817@student.saxion.nl>
2021-03-04 22:06:06 +0100

    first

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return "hello world"
+    return HttpResponse("hello world")
