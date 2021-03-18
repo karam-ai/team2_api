b68d4cf8adef36042436d5ff3387c491d1ea10d3
karam qusai <483817@student.saxion.nl>
2021-03-18 09:55:25 +0100

    adding test rout

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -7,6 +9,17 @@ from django.views.decorators.csrf import csrf_exempt
 def index(request):
     return HttpResponse("hello world")
 
+
 @csrf_exempt
 def test(request):
-    return HttpResponse("posted data: \n" + request.POST.items())
+
+    if request.POST:
+        data = request.body
+        data = '{"' + data.decode('utf-8').replace('=', '":"') + '"}'
+        data = data.replace('&', '","')
+        print(type(data), data)
+        data = json.loads(data)
+        print(data)
+        return JsonResponse(data,safe=False)
+    else:
+        return HttpResponse("you used GET, please use POST")
